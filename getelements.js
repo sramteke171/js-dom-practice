2f3b7a1b18f61e13ebc1c384d235f9b6a6bf5547
Sachin Ramteke <sachin06jd@gmail.com>
2020-07-02 10:22:42 -0500

    query selector

--- getelements.js
+++ getelements.js
@@ -5,6 +5,7 @@
  *   - use the getElement(s) methods to retrieve elements from the DOM
  */
 
+
 /*
  * You Do: Work through the following prompts. Write your code below each
  * comment.
@@ -12,14 +13,25 @@
 
 // 1. Get the element with an ID of 'first'
 
+// let titleElement = document.getElementById("title");
+// let paragraphElements = document.getElementsByClassName("paragraph");
+// let spanElements = document.getElementsByTagName("span");
+
+const first = document.getElementById("first");
+console.log(first);
+
 // 2. Now delete that first element (in the html file), and re-run your code.
 //    What changed?
 
 // 3. Get the elements with a class of 'second' with getElementsByClassName:
+const second = document.getElementsByClassName("second");
+console.log(second);
 
 // 4. Delete one of the elements with a class of 'second' and refresh the page.
 //    Do you get a similar result?
 
 // 5. Get the span element using getElementsByTagname:
+const span = document.getElementsByTagName("span");
+console.log(span);
 
 // 6. Add a span anywhere, and re-run the last piece of code. What's different?
--- queryselector.js
+++ queryselector.js
@@ -11,18 +11,38 @@
  */
 
 // 1. Get the element with an ID of 'first' using querySelector
+const first = document.querySelector('#first');
+console.log(first);
 
 // 2. Get the elements with a class of 'second' with querySelector
+// const second = document.querySelector('.second');
+// console.log(second);
 
 // 3. Try the above prompt with querySelectorAll. What's the difference between
 //    what these two methods return?
 
+const second = document.querySelectorAll('.second');
+console.log(second);
+
 // 4. Get the span element using querySelector
+const span = document.querySelector('span');
+console.log(span);
 
 // 5. Get multiple span elements using querySelectorAll
+// const span = document.querySelectorAll('span');
+// console.log(span);
 
 // 6. Select only "a" tags *directly inside* of a div (no grandchildren).
 
+const a = document.querySelectorAll('div > a');
+// const a = document.querySelectorAll('div > section > a');
+console.log(a);
+
 // 7. Select all elements that contain a "data-target" attribute
 
+// const data_target = document.querySelectorAll('[data-target]');
+// console.log(data_target);
+
 // 8. Select all elements where the data-target attribute equals "#false"
+const false_data_target = document.querySelectorAll('[data-target="#false"]');
+console.log(false_data_target);
